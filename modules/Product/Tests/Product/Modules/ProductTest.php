1af1e946c89fe52d67178c835c5b9d8f765f3686
impruthvi <pruthvirajput97@gmail.com>
2025-04-22 14:24:56 +0530

    feat: Create ProductFactory

--- modules/Product/Tests/Product/Modules/ProductTest.php
+++ modules/Product/Tests/Product/Modules/ProductTest.php
@@ -2,15 +2,21 @@
 
 namespace Modules\Order\Tests\Product\Modules;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Modules\Product\Models\Product;
 use Modules\Product\Tests\ProductTestCase;
 
 class ProductTest extends ProductTestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        $product = Product::factory()->create();
+
+        dd($product->toArray());
         $response = $this->get('/');
 
         $response->assertStatus(200);
