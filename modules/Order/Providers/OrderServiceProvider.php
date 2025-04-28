b8be04a8294b032724329b475a557f08f7bd4c20
impruthvi <pruthvirajput97@gmail.com>
2025-04-28 12:50:31 +0530

    feat: add Alert view component and integrate it into the checkout page; register Blade component paths in OrderServiceProvider

--- modules/Order/Providers/OrderServiceProvider.php
+++ modules/Order/Providers/OrderServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Order\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class OrderServiceProvider extends ServiceProvider
@@ -14,6 +15,10 @@ class OrderServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
         $this->mergeConfigFrom(__DIR__.'/../config.php', 'order');
         $this->loadRoutesFrom(__DIR__.'/../routes.php');
+        $this->loadViewsFrom(__DIR__.'/../Views', 'order');
         $this->app->register(EventServiceProvider::class);
+
+        Blade::anonymousComponentPath(__DIR__.'/../Views/components', 'order');
+        Blade::componentNamespace('Modules\Order\ViewComponents', 'order');
     }
 }
---
+++ modules/Order/Views/checkout/index.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Checkout</h1>
+                <p>Welcome to the checkout page!</p>
+            </div>
+            <x-order::alert message="This is an alert message!" />
+
+            <x-order::order-line product="Test Product" />
+        </div>
+    </div>
+</div>
